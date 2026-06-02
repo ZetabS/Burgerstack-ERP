a93f08a1796609cff50e140ccaf070456d750cb7
김유화 <ua0731@gmail.com>
2026-06-02 09:07:02 +0900

    공통 CSS  및 HO 자재
    - 공통 CSS 마무리
    - 자재 관련 테이블 일부 수정 : 단가 -> 원가 / 판매가
    - 자재 등록하기, 자재 전체보기 및 상세보기 프론트엔드 완료
    - 자재 등록하기 백엔드 작업중

--- BurgerStack_Project/src/main/java/com/kh/burgerstack/material/model/service/MaterialSerivce.java
+++ BurgerStack_Project/src/main/java/com/kh/burgerstack/material/model/service/MaterialSerivce.java
@@ -1,5 +1,26 @@
 package com.kh.burgerstack.material.model.service;
 
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.kh.burgerstack.material.model.dao.MaterialDao;
+import com.kh.burgerstack.material.model.vo.Material;
+
+@Service
 public class MaterialSerivce {
 
+	@Autowired
+	private MaterialDao materialDao;
+	@Autowired
+	private SqlSessionTemplate sqlSession;
+	
+	@Transactional
+	public int materialInsert(Material m) {
+		
+		return materialDao.materialInsert(sqlSession, m);
+	} //materialInsert
+	
+	
 }
---
+++ sql/자재SQL.sql
@@ -0,0 +1,43 @@
+-- 자재 사진 등록 쿼리문
+-- 파일 JAVA 완성 후 삽입
+INSERT INTO FILES(FILE_ID
+                , ORIGINAL_NAME
+                , STORED_NAME
+                , STORAGE_PATH
+                , MIME_TYPE
+                , FILE_SIZE
+                , CREATED_BY
+                , CREATED_AT)
+           VALUES(SEQ_FILE.NEXTVAL
+                , ?
+                , ?
+                , ?
+                , ?
+                , ?
+                , ?
+                , ?);
+-- 자재 채번 쿼리문
+
+
+-- 자재 등록 쿼리문
+INSERT INTO MATERIALS(MATERIAL_ID
+                    , MATERIAL_CODE
+                    , MATERIAL_NAME
+                    , MATERIAL_TYPE
+                    , COST_PRICE
+                    , SELLING_PRICE
+                    , IMAGE_FILE_ID
+                    , DETAILS
+                    , CREATED_BY)
+               VALUES(SEQ_MATERIAL.NEXTVAL
+                    , ? || '-' || LPAD(SEQ_MATERIAL.CURRVAL, 3, '0')
+                    , ?
+                    , ?
+                    , ?
+                    , ?
+                    , ?
+                    , ?
+                    , ?);
+
+
+-- 자재 목록 조회 쿼리문
