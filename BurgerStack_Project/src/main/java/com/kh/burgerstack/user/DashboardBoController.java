e10f360ce43c6df44439abc9895272f162c6d199
Gyumin7870 <kgmlove230@gmail.com>
2026-06-02 19:08:51 +0900

    계정등록 HO BO 까지
    컨트롤러 껍데기만 만들었어요

---
+++ BurgerStack_Project/src/main/java/com/kh/burgerstack/user/DashboardBoController.java
@@ -0,0 +1,16 @@
+package com.kh.burgerstack.user;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("user")
+public class DashboardBoController {
+
+	@GetMapping("dashboardBo")
+	public String dashboardBo() {
+		System.out.println("잘 호출 되나..?");
+		return "user/dashboardBo";
+	}
+}
