5680be49f1b4d5e6a384c3702ebf8e4f68142222
Gyumin7870 <kgmlove230@gmail.com>
2026-06-02 16:36:19 +0900

    로그인 기능, 로그아웃, 계정 등록 기능
    로그인 기능은 관리자 페이지로 가는 부분은 아직 미구현, 로그아웃 기능 구현, 계정 등록 기능 구현

--- BurgerStack_Project/src/main/java/com/kh/burgerstack/user/UserController.java
+++ BurgerStack_Project/src/main/java/com/kh/burgerstack/user/UserController.java
@@ -1,5 +1,55 @@
 package com.kh.burgerstack.user;
 
-public class UserController {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+import jakarta.servlet.http.HttpSession;
+
+@Controller
+@RequestMapping("user")
+public class UserController {
+	
+	@Autowired
+	private BCryptPasswordEncoder bCryptPasswordEncoder;
+	
+	@Autowired
+	private UserService userService;
+	
+	@GetMapping("enrollForm")
+	public String storeOwnerEnrollForm() {
+		
+		System.out.println("잘 출력되나..?");
+		
+		return "user/userEnrollForm";
+	}
+	
+	@PostMapping("insertStoreOwner")
+	public String insertStoreOwner(User u, Model model, HttpSession session) {
+		
+		String encPwd = bCryptPasswordEncoder.encode(u.getPassword());
+		
+		u.setPassword(
+				bCryptPasswordEncoder.encode(u.getPassword())
+		);
+
+		int result = userService.insertStoreOwner(u);
+		
+		if(result > 0) {
+		    session.setAttribute("alertMsg", "계정이 등록되었습니다.");
+		    
+		    return "redirect:/user/myPage";
+		} else {
+			model.addAttribute("errorMsg", "회원가입에 실패했습니다");
+			return "common/errorPage";
+			
+		}
+    } 
+	
+	
+	
 }
