5823afc2595bb3667c5e8b54092c9fea4c10e196
Gyumin7870 <kgmlove230@gmail.com>
2026-06-05 14:07:52 +0900

    계정 정보 수정, 비밀번호 변경
    관리자꺼를 안해서 다시 올립니다.

--- BurgerStack_Project/src/main/java/com/kh/burgerstack/user/AdminService.java
+++ BurgerStack_Project/src/main/java/com/kh/burgerstack/user/AdminService.java
@@ -1,5 +1,25 @@
 package com.kh.burgerstack.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
 public class AdminService {
+
+	@Autowired
+	private AdminDao adminDao;
+	
+	public int update(User u) {
+		
+		return adminDao.update(u);
+	}
+
+	public int updatePassword(User u) {
+		
+		return adminDao.updatePassword(u);
+	}
 
 }
---
+++ BurgerStack_Project/src/main/webapp/WEB-INF/views/user/OwnerList.jsp
@@ -0,0 +1,16 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+
+	<t:menubarHO>
+	<h1>점주 계정 목록 조회</h1>
+	</t:menubarHO>
+
+</body>
+</html>
